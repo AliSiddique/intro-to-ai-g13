d258f9ad4f1a3244ba326baa4dd30f8983fc410c
AliSiddique <alisiddique10@hotmail.com>
2023-12-12 14:55:33 +0000

    AS

---
+++ implementation/RandomForest.py
@@ -0,0 +1,43 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, f1_score, recall_score
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+spotify_df = pd.read_csv('../spotify_data.csv')
+selected_features = ['energy', 'loudness', 'liveness', 'instrumentalness', 'danceability']
+spotify_df = spotify_df[selected_features]
+
+# Normalise the data using Min-Max scaling
+scaler = MinMaxScaler()
+spotify_df[selected_features[:-1]] = scaler.fit_transform(spotify_df[selected_features[:-1]])
+
+# Split data into training and testing sets
+X = spotify_df[selected_features[:-1]]
+y = spotify_df['danceability']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Convert labels to binary classification (0 or 1) based on threshold
+threshold = 0.5
+y_train_binary = np.where(y_train > threshold, 1, 0)
+y_test_binary = np.where(y_test > threshold, 1, 0)
+
+# Initialize Random Forest Classifier
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the model
+rf_classifier.fit(X_train, y_train_binary)
+
+# Predict on test set
+y_pred_rf = rf_classifier.predict(X_test)
+
+# Evaluate model
+accuracy_rf = accuracy_score(y_test_binary, y_pred_rf)
+f1_rf = f1_score(y_test_binary, y_pred_rf)
+recall_rf = recall_score(y_test_binary, y_pred_rf)
+
+# Print metrics
+print(f"Accuracy (Random Forest): {accuracy_rf}")
+
