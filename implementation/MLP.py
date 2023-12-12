cd4ac1883ce3e04eebba85659d3d5e24ce3d8b0b
AliSiddique <alisiddique10@hotmail.com>
2023-12-12 14:58:22 +0000

    AS

--- implementation/MLP.py
+++ implementation/MLP.py
@@ -16,3 +16,16 @@ import matplotlib.pyplot as plt
 # Turn data into tensors
 spotify_df = pd.read_csv("../spotify_data.csv",encoding="ISO-8859-1")
 spotify_df.head()
+
+
+selected_features = ['energy', 'loudness', 'liveness', 'instrumentalness', 'danceability']
+spotify_df = spotify_df[selected_features]
+
+# Normalise the data using Min-Max scaling
+scaler = MinMaxScaler()
+spotify_df[selected_features[:-1]] = scaler.fit_transform(spotify_df[selected_features[:-1]])
+
+# Split data into training and testing sets
+X = spotify_df[selected_features[:-1]]
+y = spotify_df['danceability']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
