08adf6f9abd845f166992fd90cde2a05c329db75
AliSiddique <alisiddique10@hotmail.com>
2024-01-07 11:40:58 +0000

    DA

--- implementation/Data-cleaning.py
+++ implementation/Data-cleaning.py
@@ -13,3 +13,13 @@ df.set_index('track_id', inplace=True)
 null_values = df.isnull()
 null_counts = null_values.sum()
 df = df.head(150000)
+
+
+
+# Set threshold
+threshold = 0.5
+
+# Apply lambda function to update danceability values
+df['danceability'] = df['danceability'].apply(lambda x: 1 if x >= threshold else 0)
+# Make a new csv file with the change data
+df.to_csv("../updated_spotify_data.csv", index=False)
